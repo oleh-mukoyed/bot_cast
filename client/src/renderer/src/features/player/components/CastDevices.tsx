c5f94c8c44cbf59006cac5d033ba6b678a1541f5
Oleh Mukoyed <oleh.mukoyed@gmail.com>
2023-10-23 16:12:52 +0300

    fix: refresh the devices list after menu showed

--- client/src/renderer/src/features/player/components/CastDevices.tsx
+++ client/src/renderer/src/features/player/components/CastDevices.tsx
@@ -8,11 +8,14 @@ import { Fragment, useState } from 'react'
 export function CastDevices(): JSX.Element {
   const { castTV, getDevices, connectToDevice, disconnectFromDevice, isProcess } = useConnect()
   const [devices, setDevices] = useState<Array<string>>([])
+  const [listReady, setListReady] = useState(false)
 
   const connectClass = castTV ? 'text-green-600 hover:text-green-700' : ''
 
   const menuClick = async () => {
+    if (listReady) return
     setDevices(await getDevices())
+    setListReady(true)
   }
 
   const castList = castTV ? (
@@ -79,20 +82,21 @@ export function CastDevices(): JSX.Element {
           />
         </Menu.Button>
       </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-base-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-d_base-200">
-          <div className="py-1">{castList}</div>
-        </Menu.Items>
-      </Transition>
+      {listReady && (
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-base-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-d_base-200">
+            <div className="py-1">{castList}</div>
+          </Menu.Items>
+        </Transition>
+      )}
     </Menu>
   )
 }
